358023dd334fe59ccf6026a95686f04aef81a540
Rashid Mohamad <rashid2538@gmail.com>
2025-05-21 21:00:11 +0530

    Refactor main.py to improve model loading and image generation messages; add output images and update numpy installation command in readme.md.

--- main.py
+++ main.py
@@ -9,30 +9,28 @@ num_images = 1
 width = 640 # Should be a multiple of 8
 height = 360 # Should be a multiple of 8
 
-# Set device to CPU (your Mac doesn't have CUDA/GPU support)
-device = "cpu" #cpu/xpu
-
 # Load the model - OFA-Sys small Stable Diffusion
 model_id = "Lykon/dreamshaper-6" # "OFA-Sys/small-stable-diffusion-v0"
 
-print("Loading the model. This may take a while...")
+print("Loading the model. This may take a while ...")
 pipe = StableDiffusionPipeline.from_pretrained(
     model_id,
     torch_dtype=torch.float32,
     safety_checker=None  # Optional: disable NSFW filter to save resources
 )
-pipe = pipe.to(device)
+pipe = pipe.to("cpu")
+print("Model loaded ...")
 
 def generate_image(prompt, output, steps=25):
-    print("Generating image...")
+    print("Generating image ...")
     image = pipe(prompt, num_inference_steps=steps, width=width, height=height, num_images_per_prompt=num_images).images[0]
     image.save(output)
-    print(f"Image saved to {output}")
+    print(f"Image saved to {output} ...")
 
 if len(sys.argv) > 2:
     inputFile = sys.argv[1]
     outputPath = sys.argv[2]
-    print(f"Input: {input}")
+    print(f"Input file : {input}")
 
     if Path(inputFile).is_file():
         Path(outputPath).mkdir(parents=True, exist_ok=True)
